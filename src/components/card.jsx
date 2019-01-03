51e10d5abc572e6629c5d7b8e47645717a677357
Lebranc Convas <33986491+lebrancconvas@users.noreply.github.com>
2019-01-03 16:29:00 +0700

    :tada: Create Repository.

---
+++ src/components/card.jsx
@@ -0,0 +1,11 @@
+import React , {Component} from 'react'
+
+export default class Card extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Card Slot Empty</h1>
+            </div>
+        )
+    }
+}
